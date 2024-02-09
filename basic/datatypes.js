83c54983cfe9229fe854e5edc3873fba0eb81d3c
sudhanshu808-in <149045400+sudhanshu808-in@users.noreply.github.com>
2024-02-09 15:55:43 +0000

    updates

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -1,4 +1,5 @@
 // alert(3+3)
+//premitive data  types
 let ans= true
 let apple="apple"
 let no=12
@@ -7,3 +8,11 @@ let s=undefined
 let k=BigInt
 console.log(typeof undefined);//undefined
 console.log(typeof null);//object
+const a=87457245692734569237459n //this n -> will make it BigInt
+//refrence data types
+//ARRAYS , OBJECTS AND FUNCTIONS
+const niggas=["Travis","thug","Metroooo....","slime"]
+let object={
+    name : "shubham",
+    roll : 12312,
+}
