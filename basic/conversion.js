2baed06370dd1062da5b58d98f7dbfe619c28b69
sudhanshu808-in <149045400+sudhanshu808-in@users.noreply.github.com>
2024-02-09 06:22:25 +0000

    conversions of dataTypes

---
+++ basic/conversion.js
@@ -0,0 +1,29 @@
+// let score = "33"
+// console.log(typeof score);
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// let score = "33abc"
+// console.log(typeof score);
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// let score = null
+// console.log(typeof score);
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// let score = true
+// console.log(typeof score);
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+let num=8968
+let convertedNum=String(num);
+console.log(convertedNum);
+console.log(typeof convertedNum);
