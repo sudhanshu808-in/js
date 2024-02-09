879805da0a804cec91af68cd4ca13a61f50a1799
sudhanshu808-in <149045400+sudhanshu808-in@users.noreply.github.com>
2024-02-09 06:06:29 +0000

    datatypes and variables

---
+++ basic/datatypes.js
@@ -0,0 +1,9 @@
+// alert(3+3)
+let ans= true
+let apple="apple"
+let no=12
+let state =null
+let s=undefined
+let k=BigInt
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
---
+++ basic/variables.js
@@ -0,0 +1,7 @@
+const accountId=45654
+let email="xyz@gamil.com"
+var pass="23409"
+city="dehradun"
+//var had scope problem so dont use it just  use let instead
+
+console.table([accountId,email,pass,city])
