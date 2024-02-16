41560791df915c3f407d7a4e2d66b21502aea13f
sudhanshu808-in <149045400+sudhanshu808-in@users.noreply.github.com>
2024-02-16 15:47:36 +0000

    stack vs heap

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -8,7 +8,7 @@ let s=undefined
 let k=BigInt
 console.log(typeof undefined);//undefined
 console.log(typeof null);//object
-const a=87457245692734569237459n //this n -> will make it BigInt
+// const a=87457245692734569237459n //this n -> will make it BigInt
 //refrence data types
 //ARRAYS , OBJECTS AND FUNCTIONS
 const niggas=["Travis","thug","Metroooo....","slime"]
@@ -16,3 +16,22 @@ let object={
     name : "shubham",
     roll : 12312,
 }
+const fun = function(){
+    console.log("nigga what up");
+}
+
+// <<<<<<<<<<<STACK VS HEAP>>>>>>>>>>>>>
+let a="nigga"
+let b=a
+a="boi what the hell"
+console.log(a);
+console.log(b);
+let obj={
+    boi : "apple",
+
+}
+let obj2=obj;
+obj2.boi="mango"
+console.log(obj)
+console.log(obj2)
+
