abc65823e7986c8fe72de1319c93fa6c38bb5f46
sudhanshu808-in <149045400+sudhanshu808-in@users.noreply.github.com>
2024-02-17 14:58:09 +0000

    add new files

--- basic/comparison.js
+++ basic/comparison.js
@@ -1,3 +1,24 @@
 console.log(2==1);
 console.log("2">"4");
 console.log("2"===2);
+
+const addingTwoNumber = (marks) => {
+    let grade = '';
+    if(marks>=75){
+         grade = "d"
+    }
+    else if(marks>= 60 && marks <=70){
+        grade="first"
+    }
+    else if(marks>45  && marks <60){
+        grade="second"
+    }
+    else if(marks>= 33 && marks <=45){
+        grade="third"
+    }
+    else {
+        grade="failed"
+    }
+    return grade;
+}
+addingTwoNumber(65)
