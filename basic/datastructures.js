6d663414d4159cc60d774ce078c0cb07a2625557
sudhanshu808-in <149045400+sudhanshu808-in@users.noreply.github.com>
2024-02-21 15:52:48 +0000

    arrays and strings

---
+++ basic/datastructures.js
@@ -0,0 +1,21 @@
+const arr=[1,2,3,4,5]
+const heroes=["alphaBOI","Batman","ooooooh"]
+const arr2= new Array(1,2,3,4,5)
+const milawat=[...arr,...heroes,...arr2]
+// console.log(milawat)
+// console.log(arr[0])
+// arr.push(69)
+// console.log(arr)
+// console.log(arr.includes(9))
+// console.log(arr.indexOf(3))
+
+
+//slice vs splice
+console.log(arr.slice(1,4))
+console.log(`original array ${arr}`)
+console.log(arr.splice(1,4))
+console.log(`original array ${arr}`)
+
+
+//faltining 
+const arr3=[1,2,3,[4,5,6],7[8,9,[10]]]
---
+++ basic/strings.js
@@ -0,0 +1,18 @@
+const name="hitesh"
+const repo=34
+const str="HELLO MY NAME IS SHUBHAM"
+console.log(`the name of the persong is ${name} and they have a repo of ${repo}`);
+console.log(str.toLowerCase());
+console.log(str);
+console.log(str.charAt(6));
+console.log(str.indexOf('H'));
+const substr=str.substring(4,15)
+const sloice=str.slice(-4,2)
+console.log(substr);
+console.log(sloice);
+const ok= "    abc           "
+
+console.log(ok.trim());
+const url="https://shubham.com/shubham-info"
+
+
