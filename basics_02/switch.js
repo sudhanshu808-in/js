d4851e30dd88c34941aefb3c73b8043acd6068b2
sudhanshu808-in <149045400+sudhanshu808-in@users.noreply.github.com>
2024-02-25 15:16:36 +0000

    Switch case

---
+++ basics_02/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "octo"
+switch(month){
+    case "october":
+        console.log("no");
+        break;
+    case "january":
+        console.log("no");
+    case "march":
+        console.log("ok matched now what ??");
+    default :
+    console.log("Nothing is there , nothing to do");
+    break;
+}
+const number=2
+switch(number){
+    case 1 :
+        console.log(`No is found ${number}`);
+    break;
+    case 2 :
+        console.log(`No is found ${number}`);
+    break;
+    default :
+    console.log("nothing matched");
+}
